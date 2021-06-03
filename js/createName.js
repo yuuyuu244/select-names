7193956d7a99d355b717d11e816a3ae9ea3fa647
ocs194069 <194069@ocsjoho.onmicrosoft.com>
2021-06-03 11:46:35 +0900

    use json file to classNames

--- js/createName.js
+++ js/createName.js
@@ -5,55 +5,66 @@
  */
 // var isClick = false;
 // var cnt = 0;
-const classNameOfFirst = "template";
+const classNameOfFirst = 'template';
 const classNamesForLoading = [
-	"template",
-	"template",
-	"template"
-	// "2021h_FE24", 	/* 国対2021年度 FE24 */
-	// "2021_R3A",		/* 2021年度 大学併修学科A */
-	// "2021_R3B",		/* 2021年度 大学併修学科B */
-	// "2021_R3"		/* 2021年度 大学併修学科ALL */
+	'template1',
+	'template2',
+	// '2021h_FE24', 	/* 国対2021年度 FE24 */
+	// '2021_R3A',		/* 2021年度 大学併修学科A */
+	// '2021_R3B',		/* 2021年度 大学併修学科B */
+	// '2021_R3'		/* 2021年度 大学併修学科ALL */
 ];
+// name
+let tempNameList = {};
+let clickflow = null;
 
 /**
  * load the name argument.
  * @param {string} className 
  */
-function loadClass(className){
-		var script = document.createElement('script');
-		script.src = "./js/" + className + ".js";
-		document.head.appendChild(script);
+function loadClass(className) {
+	var script = document.createElement('script');
+	script.src = './js/' + className + '.js';
+	document.head.appendChild(script);
 }
 
-/**
- * load the default of name.  
- */
-loadClass(classNameOfFirst);
+function getJSON(filepath) {
+	const xhr = new XMLHttpRequest();
+	xhr.open('GET', chrome.runtime.getURL(filepath), false);
+	xhr.send(null);
 
-let clickflow = null;
+	if (xhr.status == 200) {
+		return JSON.parse(xhr.response);
+	}
+}
 
 /**
  * Trigger DOM Content Loaded
  */
-document.addEventListener("DOMContentLoaded", () => {
-    // OnClickListener
-	// id of "stop" button
-	document.getElementById("start").addEventListener("click", () => {
-	    console.log("click select");
-	    isClick = false;
-		if(clickflow===null) {
-			clickflow = setInterval(()=> {
-					let idx = Math.floor(Math.random() * names.length);
-					document.getElementById("ChromePlugin-result-disp").innerHTML = names[idx];
-			},1);
+document.addEventListener('DOMContentLoaded', () => {
+	// OnClickListener
+	// id of 'stop' button
+	document.getElementById('start').addEventListener('click', () => {
+		console.log('click select');
+
+		let nameList = [];
+		for (let key in tempNameList) {
+			nameList = nameList.concat(tempNameList[key]);
+		}
+
+		isClick = false;
+		if (clickflow === null) {
+			clickflow = setInterval(() => {
+				let idx = Math.floor(Math.random() * nameList.length);
+				document.getElementById('ChromePlugin-result-disp').innerHTML = nameList[idx];
+			}, 1);
 		}
 	});
 
-    // OnClickListener
-	// id of "stop" button
-	document.getElementById("stop").addEventListener("click", () => {
-	    console.log("clicked stop");
+	// OnClickListener
+	// id of 'stop' button
+	document.getElementById('stop').addEventListener('click', () => {
+		console.log('clicked stop');
 		if (clickflow) {
 			clearInterval(clickflow);
 			clickflow = null;
@@ -61,9 +72,24 @@ document.addEventListener("DOMContentLoaded", () => {
 	});
 	// OnClickListener
 	// set the Listener of class names of html id
-	for(let id of classNamesForLoading) {
-			document.getElementById(id).addEventListener("click", ()=>{
-				loadClass(id);
-			});
+	// for (let id of classNamesForLoading) {
+	// 	document.getElementById(id).addEventListener('click', () => {
+	// 		loadClass(id);
+	// 	});
+	// }
+
+	for (let id of classNamesForLoading) {
+		let checkbox = document.getElementById(id);
+		checkbox.addEventListener('click', () => {
+			if (checkbox.checked) {
+				console.log('--checked--');
+				const json = getJSON('json/' + id + '.json');
+				tempNameList[id] = json.names;
+			} else {
+				console.log('--unchecked--');
+				delete tempNameList[id];
+			}
+			console.log(tempNameList);
+		});
 	}
 });
