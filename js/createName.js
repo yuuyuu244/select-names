5efcdc41bc30af9b2a01d36f74eaa493cfcc48e9
ocs194069 <194069@ocsjoho.onmicrosoft.com>
2021-06-04 23:15:02 +0900

    added "all check" and "all uncheck" buttons and other improvements

--- js/createName.js
+++ js/createName.js
@@ -3,88 +3,98 @@
  * $Date$
  * $Author$ : Yuki-Kikuya
  */
-// var isClick = false;
-// var cnt = 0;
-const classNameOfFirst = 'template';
-const classNamesForLoading = [
-	'template1',
-	'template2',
-	// '2021h_FE24', 	/* 国対2021年度 FE24 */
-	// '2021_R3A',		/* 2021年度 大学併修学科A */
-	// '2021_R3B',		/* 2021年度 大学併修学科B */
-	// '2021_R3'		/* 2021年度 大学併修学科ALL */
+
+const fileNamesForLoading = [
+    'template',
+    'template2',
 ];
-// name
-let tempNameList = {};
+
+let checkboxes = [];
+let fileDictionary = {};
 let clickflow = null;
 
-/**
- * load the name argument.
- * @param {string} className 
- */
-function loadClass(className) {
-	var script = document.createElement('script');
-	script.src = './js/' + className + '.js';
-	document.head.appendChild(script);
-}
-
-async function getJSON(filepath) {
-	const response = await fetch(chrome.runtime.getURL(filepath));
-	const json = await response.json();
-	return json;
-}
-
-/**
- * Trigger DOM Content Loaded
- */
 document.addEventListener('DOMContentLoaded', () => {
-	// OnClickListener
-	// id of 'stop' button
-	document.getElementById('start').addEventListener('click', () => {
-		console.log('click select');
+    init();
 
-		let nameList = [];
-		for (let key in tempNameList) {
-			nameList = nameList.concat(tempNameList[key]);
-		}
+    document.getElementById('check-all').addEventListener('click', () => {
+        for (let checkbox of checkboxes) {
+            checkbox.checked = true;
+        }
+    });
 
-		if (clickflow === null) {
-			clickflow = setInterval(() => {
-				let idx = Math.floor(Math.random() * nameList.length);
-				document.getElementById('ChromePlugin-result-disp').innerHTML = nameList[idx];
-			}, 1);
-		}
-	});
+    document.getElementById('uncheck-all').addEventListener('click', () => {
+        for (let checkbox of checkboxes) {
+            checkbox.checked = false;
+        }
+    });
 
-	// OnClickListener
-	// id of 'stop' button
-	document.getElementById('stop').addEventListener('click', () => {
-		console.log('clicked stop');
-		if (clickflow) {
-			clearInterval(clickflow);
-			clickflow = null;
-		}
-	});
-	// OnClickListener
-	// set the Listener of class names of html id
-	// for (let id of classNamesForLoading) {
-	// 	document.getElementById(id).addEventListener('click', () => {
-	// 		loadClass(id);
-	// 	});
-	// }
+    document.getElementById('start').addEventListener('click', () => {
+        console.log('start clicked');
+        let nameList = [];
+        for (checkbox of checkboxes) {
+            if (checkbox.checked) {
+                const id = checkbox.id;
+                nameList = nameList.concat(fileDictionary[id]);
+            }
+        }
+        console.log(nameList);
 
-	for (let id of classNamesForLoading) {
-		let checkbox = document.getElementById(id);
-		checkbox.addEventListener('click', async () => {
-			if (checkbox.checked) {
-				console.log('--checked--');
-				const json = await getJSON('json/' + id + '.json');
-				tempNameList[id] = json.names;
-			} else {
-				console.log('--unchecked--');
-				delete tempNameList[id];
-			}
-			console.log(tempNameList);
-		});
-	}
+        if (clickflow === null && nameList.length) {
+            console.log('start roulette');
+            clickflow = setInterval(() => {
+                const idx = Math.floor(Math.random() * nameList.length);
+                document.getElementById('ChromePlugin-result-disp').innerHTML = nameList[idx];
+            }, 10);
+        } else {
+            console.log('not start roulette');
+        }
+    });
+
+    document.getElementById('stop').addEventListener('click', () => {
+        console.log('stop clicked');
+        if (clickflow) {
+            clearInterval(clickflow);
+            clickflow = null;
+            console.log('stop roulette');
+        }
+    });
 });
+
+/**
+ * Initialize fileDictionary and create html elements
+ */
+async function init() {
+    for (const fileName of fileNamesForLoading) {
+        const json = await getJSON('json/' + fileName + '.json');
+        fileDictionary[fileName] = json.names;
+        createCheckbox(fileName, json.className);
+    }
+}
+
+/**
+ * get JSON file
+ * @param {string} filePath 
+ * @returns {Object} parsed JSON
+ */
+async function getJSON(filePath) {
+    const response = await fetch(chrome.runtime.getURL(filePath));
+    const json = await response.json();
+    return json;
+}
+
+function createCheckbox(id, innerText) {
+    const checkbox = document.createElement('input');
+    const label = document.createElement('label');
+
+    checkbox.type = 'checkbox';
+    checkbox.id = id;
+
+    label.htmlFor = id;
+    label.innerText = innerText;
+
+    checkboxes.push(checkbox);
+
+    const div = document.getElementById('checkbox-area');
+    div.appendChild(checkbox);
+    div.appendChild(label);
+}
