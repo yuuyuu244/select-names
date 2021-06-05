b1d073405a9e4129a959bd16a80c8e1080da3f31
Yuki Kikuya <yuuyuu244@yahoo.co.jp>
2021-06-05 14:44:20 +0900

    Revert "ファイル読込方法・機能の大規模変更"

--- README.md
+++ README.md
@@ -2,32 +2,21 @@
 
 ## Usage
 
-1. create the json of yours in `./json/[any name].json`  
+1. create the array(s) of yours in `./js/[any your original name 1].js`  
+   default js file name is `templete.js`. so you should modify this.  
+2. change items of `classNamesForLoading` array in `./js/createName.js`  
    e.g.
 ```
-{
-    "className": "it will display on popup",
-    "names": [
-        "A",
-        "B",
-        "C"
-    ]
-}
-```
-   default json file name is `template.json`. so you should modify this.  
-   
-2. change items of `fileNamesForLoading` array in `./js/createName.js`  
-   e.g.
-```
-const fileNamesForLoading = [
-   '[your original 1]',
-   '[your original 2]',
-   '[your original 3]',
+const classNamesForLoading = [
+	"[any your original name 1]",
+	"[any your original name 2]",
+	"[any your original name 3]"
 ];
 ```
-3. access `chrome://extensions` on Google Chrome  
-   you should switch ON of 'developer mode'
-4. drag this project file from root(ALL files) and drop into `chrome://extensions` page
+3. change tags of id in `.classes` in `popup.html`  
+4. access `chrome://extensions` on Google Chrome    
+   you should switch ON of 'developer mode'  
+5. drag this project file from root(ALL files) and drop into `chrome://extensions` page  
 
 ## Future Prospects
 
--- js/createName.js
+++ js/createName.js
@@ -3,98 +3,67 @@
  * $Date$
  * $Author$ : Yuki-Kikuya
  */
-
-const fileNamesForLoading = [
-    'template',
-    'template2',
+// var isClick = false;
+// var cnt = 0;
+const classNameOfFirst = "template";
+const classNamesForLoading = [
+	"template",
+	"template",
+	"template"
+	// "2021h_FE24", 	/* 国対2021年度 FE24 */
+	// "2021_R3A",		/* 2021年度 大学併修学科A */
+	// "2021_R3B",		/* 2021年度 大学併修学科B */
+	// "2021_R3"		/* 2021年度 大学併修学科ALL */
 ];
 
-let checkboxes = [];
-let fileDictionary = {};
+/**
+ * load the name argument.
+ * @param {string} className 
+ */
+function loadClass(className){
+		var script = document.createElement('script');
+		script.src = "./js/" + className + ".js";
+		document.head.appendChild(script);
+}
+
+/**
+ * load the default of name.  
+ */
+loadClass(classNameOfFirst);
+
 let clickflow = null;
 
-document.addEventListener('DOMContentLoaded', () => {
-    init();
+/**
+ * Trigger DOM Content Loaded
+ */
+document.addEventListener("DOMContentLoaded", () => {
+    // OnClickListener
+	// id of "stop" button
+	document.getElementById("start").addEventListener("click", () => {
+	    console.log("click select");
+	    isClick = false;
+		if(clickflow===null) {
+			clickflow = setInterval(()=> {
+					let idx = Math.floor(Math.random() * names.length);
+					document.getElementById("ChromePlugin-result-disp").innerHTML = names[idx];
+			},1);
+		}
+	});
 
-    document.getElementById('check-all').addEventListener('click', () => {
-        for (let checkbox of checkboxes) {
-            checkbox.checked = true;
-        }
-    });
-
-    document.getElementById('uncheck-all').addEventListener('click', () => {
-        for (let checkbox of checkboxes) {
-            checkbox.checked = false;
-        }
-    });
-
-    document.getElementById('start').addEventListener('click', () => {
-        console.log('start clicked');
-        let nameList = [];
-        for (checkbox of checkboxes) {
-            if (checkbox.checked) {
-                const id = checkbox.id;
-                nameList = nameList.concat(fileDictionary[id]);
-            }
-        }
-        console.log(nameList);
-
-        if (clickflow === null && nameList.length) {
-            console.log('start roulette');
-            clickflow = setInterval(() => {
-                const idx = Math.floor(Math.random() * nameList.length);
-                document.getElementById('ChromePlugin-result-disp').innerHTML = nameList[idx];
-            }, 10);
-        } else {
-            console.log('not start roulette');
-        }
-    });
-
-    document.getElementById('stop').addEventListener('click', () => {
-        console.log('stop clicked');
-        if (clickflow) {
-            clearInterval(clickflow);
-            clickflow = null;
-            console.log('stop roulette');
-        }
-    });
+    // OnClickListener
+	// id of "stop" button
+	document.getElementById("stop").addEventListener("click", () => {
+	    console.log("clicked stop");
+		if (clickflow) {
+			clearInterval(clickflow);
+			clickflow = null;
+		}
+	});
+	// OnClickListener
+	// set the Listener of class names of html id
+	for(let id of classNamesForLoading) {
+			document.getElementById(id).addEventListener("click", ()=>{
+				loadClass(id);
+			});
+	}
 });
-
-/**
- * Initialize fileDictionary and create html elements
- */
-async function init() {
-    for (const fileName of fileNamesForLoading) {
-        const json = await getJSON('json/' + fileName + '.json');
-        fileDictionary[fileName] = json.names;
-        createCheckbox(fileName, json.className);
-    }
-}
-
-/**
- * get JSON file
- * @param {string} filePath 
- * @returns {Object} parsed JSON
- */
-async function getJSON(filePath) {
-    const response = await fetch(chrome.runtime.getURL(filePath));
-    const json = await response.json();
-    return json;
-}
-
-function createCheckbox(id, innerText) {
-    const checkbox = document.createElement('input');
-    const label = document.createElement('label');
-
-    checkbox.type = 'checkbox';
-    checkbox.id = id;
-
-    label.htmlFor = id;
-    label.innerText = innerText;
-
-    checkboxes.push(checkbox);
-
-    const div = document.getElementById('checkbox-area');
-    div.appendChild(checkbox);
-    div.appendChild(label);
-}
